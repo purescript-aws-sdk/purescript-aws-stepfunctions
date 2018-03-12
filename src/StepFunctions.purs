26b9bdebc6ff68d54b27ef0ac80d62376b5ce66d
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:25:59 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/StepFunctions.purs
+++ src/StepFunctions.purs
@@ -20,102 +20,138 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "StepFunctions" :: String
-
 
 -- | <p>Creates an activity. An activity is a task which you write in any programming language and host on any machine which has access to AWS Step Functions. Activities must poll Step Functions using the <code>GetActivityTask</code> API action and respond using <code>SendTask*</code> API actions. This function lets Step Functions know the existence of your activity and returns an identifier for use in a state machine and when polling from the activity.</p>
 createActivity :: forall eff. CreateActivityInput -> Aff (exception :: EXCEPTION | eff) CreateActivityOutput
-createActivity = Request.request serviceName "createActivity" 
+createActivity = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "createActivity"
 
 
 -- | <p>Creates a state machine. A state machine consists of a collection of states that can do work (<code>Task</code> states), determine to which states to transition next (<code>Choice</code> states), stop an execution with an error (<code>Fail</code> states), and so on. State machines are specified using a JSON-based, structured language.</p>
 createStateMachine :: forall eff. CreateStateMachineInput -> Aff (exception :: EXCEPTION | eff) CreateStateMachineOutput
-createStateMachine = Request.request serviceName "createStateMachine" 
+createStateMachine = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "createStateMachine"
 
 
 -- | <p>Deletes an activity.</p>
 deleteActivity :: forall eff. DeleteActivityInput -> Aff (exception :: EXCEPTION | eff) DeleteActivityOutput
-deleteActivity = Request.request serviceName "deleteActivity" 
+deleteActivity = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "deleteActivity"
 
 
 -- | <p>Deletes a state machine. This is an asynchronous operation: It sets the state machine's status to <code>DELETING</code> and begins the deletion process. Each state machine execution is deleted the next time it makes a state transition.</p> <note> <p>The state machine itself is deleted after all executions are completed or deleted.</p> </note>
 deleteStateMachine :: forall eff. DeleteStateMachineInput -> Aff (exception :: EXCEPTION | eff) DeleteStateMachineOutput
-deleteStateMachine = Request.request serviceName "deleteStateMachine" 
+deleteStateMachine = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "deleteStateMachine"
 
 
 -- | <p>Describes an activity.</p>
 describeActivity :: forall eff. DescribeActivityInput -> Aff (exception :: EXCEPTION | eff) DescribeActivityOutput
-describeActivity = Request.request serviceName "describeActivity" 
+describeActivity = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "describeActivity"
 
 
 -- | <p>Describes an execution.</p>
 describeExecution :: forall eff. DescribeExecutionInput -> Aff (exception :: EXCEPTION | eff) DescribeExecutionOutput
-describeExecution = Request.request serviceName "describeExecution" 
+describeExecution = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "describeExecution"
 
 
 -- | <p>Describes a state machine.</p>
 describeStateMachine :: forall eff. DescribeStateMachineInput -> Aff (exception :: EXCEPTION | eff) DescribeStateMachineOutput
-describeStateMachine = Request.request serviceName "describeStateMachine" 
+describeStateMachine = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "describeStateMachine"
 
 
 -- | <p>Describes the state machine associated with a specific execution.</p>
 describeStateMachineForExecution :: forall eff. DescribeStateMachineForExecutionInput -> Aff (exception :: EXCEPTION | eff) DescribeStateMachineForExecutionOutput
-describeStateMachineForExecution = Request.request serviceName "describeStateMachineForExecution" 
+describeStateMachineForExecution = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "describeStateMachineForExecution"
 
 
 -- | <p>Used by workers to retrieve a task (with the specified activity ARN) which has been scheduled for execution by a running state machine. This initiates a long poll, where the service holds the HTTP connection open and responds as soon as a task becomes available (i.e. an execution of a task of this type is needed.) The maximum time the service holds on to the request before responding is 60 seconds. If no task is available within 60 seconds, the poll returns a <code>taskToken</code> with a null string.</p> <important> <p>Workers should set their client side socket timeout to at least 65 seconds (5 seconds higher than the maximum time the service may hold the poll request).</p> </important>
 getActivityTask :: forall eff. GetActivityTaskInput -> Aff (exception :: EXCEPTION | eff) GetActivityTaskOutput
-getActivityTask = Request.request serviceName "getActivityTask" 
+getActivityTask = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "getActivityTask"
 
 
 -- | <p>Returns the history of the specified execution as a list of events. By default, the results are returned in ascending order of the <code>timeStamp</code> of the events. Use the <code>reverseOrder</code> parameter to get the latest events first.</p> <p>If a <code>nextToken</code> is returned by a previous call, there are more results available. To retrieve the next page of results, make the call again using the returned token in <code>nextToken</code>. Keep all other arguments unchanged.</p>
 getExecutionHistory :: forall eff. GetExecutionHistoryInput -> Aff (exception :: EXCEPTION | eff) GetExecutionHistoryOutput
-getExecutionHistory = Request.request serviceName "getExecutionHistory" 
+getExecutionHistory = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "getExecutionHistory"
 
 
 -- | <p>Lists the existing activities.</p> <p>If a <code>nextToken</code> is returned by a previous call, there are more results available. To retrieve the next page of results, make the call again using the returned token in <code>nextToken</code>. Keep all other arguments unchanged.</p>
 listActivities :: forall eff. ListActivitiesInput -> Aff (exception :: EXCEPTION | eff) ListActivitiesOutput
-listActivities = Request.request serviceName "listActivities" 
+listActivities = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "listActivities"
 
 
 -- | <p>Lists the executions of a state machine that meet the filtering criteria.</p> <p>If a <code>nextToken</code> is returned by a previous call, there are more results available. To retrieve the next page of results, make the call again using the returned token in <code>nextToken</code>. Keep all other arguments unchanged.</p>
 listExecutions :: forall eff. ListExecutionsInput -> Aff (exception :: EXCEPTION | eff) ListExecutionsOutput
-listExecutions = Request.request serviceName "listExecutions" 
+listExecutions = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "listExecutions"
 
 
 -- | <p>Lists the existing state machines.</p> <p>If a <code>nextToken</code> is returned by a previous call, there are more results available. To retrieve the next page of results, make the call again using the returned token in <code>nextToken</code>. Keep all other arguments unchanged.</p>
 listStateMachines :: forall eff. ListStateMachinesInput -> Aff (exception :: EXCEPTION | eff) ListStateMachinesOutput
-listStateMachines = Request.request serviceName "listStateMachines" 
+listStateMachines = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "listStateMachines"
 
 
 -- | <p>Used by workers to report that the task identified by the <code>taskToken</code> failed.</p>
 sendTaskFailure :: forall eff. SendTaskFailureInput -> Aff (exception :: EXCEPTION | eff) SendTaskFailureOutput
-sendTaskFailure = Request.request serviceName "sendTaskFailure" 
+sendTaskFailure = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "sendTaskFailure"
 
 
 -- | <p>Used by workers to report to the service that the task represented by the specified <code>taskToken</code> is still making progress. This action resets the <code>Heartbeat</code> clock. The <code>Heartbeat</code> threshold is specified in the state machine's Amazon States Language definition. This action does not in itself create an event in the execution history. However, if the task times out, the execution history contains an <code>ActivityTimedOut</code> event.</p> <note> <p>The <code>Timeout</code> of a task, defined in the state machine's Amazon States Language definition, is its maximum allowed duration, regardless of the number of <a>SendTaskHeartbeat</a> requests received.</p> </note> <note> <p>This operation is only useful for long-lived tasks to report the liveliness of the task.</p> </note>
 sendTaskHeartbeat :: forall eff. SendTaskHeartbeatInput -> Aff (exception :: EXCEPTION | eff) SendTaskHeartbeatOutput
-sendTaskHeartbeat = Request.request serviceName "sendTaskHeartbeat" 
+sendTaskHeartbeat = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "sendTaskHeartbeat"
 
 
 -- | <p>Used by workers to report that the task identified by the <code>taskToken</code> completed successfully.</p>
 sendTaskSuccess :: forall eff. SendTaskSuccessInput -> Aff (exception :: EXCEPTION | eff) SendTaskSuccessOutput
-sendTaskSuccess = Request.request serviceName "sendTaskSuccess" 
+sendTaskSuccess = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "sendTaskSuccess"
 
 
 -- | <p>Starts a state machine execution.</p>
 startExecution :: forall eff. StartExecutionInput -> Aff (exception :: EXCEPTION | eff) StartExecutionOutput
-startExecution = Request.request serviceName "startExecution" 
+startExecution = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "startExecution"
 
 
 -- | <p>Stops an execution.</p>
 stopExecution :: forall eff. StopExecutionInput -> Aff (exception :: EXCEPTION | eff) StopExecutionOutput
-stopExecution = Request.request serviceName "stopExecution" 
+stopExecution = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "stopExecution"
 
 
 -- | <p>Updates an existing state machine by modifying its <code>definition</code> and/or <code>roleArn</code>. Running executions will continue to use the previous <code>definition</code> and <code>roleArn</code>.</p> <note> <p>All <code>StartExecution</code> calls within a few seconds will use the updated <code>definition</code> and <code>roleArn</code>. Executions started immediately after calling <code>UpdateStateMachine</code> may use the previous state machine <code>definition</code> and <code>roleArn</code>. You must include at least one of <code>definition</code> or <code>roleArn</code> or you will receive a <code>MissingRequiredParameter</code> error.</p> </note>
 updateStateMachine :: forall eff. UpdateStateMachineInput -> Aff (exception :: EXCEPTION | eff) UpdateStateMachineOutput
-updateStateMachine = Request.request serviceName "updateStateMachine" 
+updateStateMachine = Request.request service method  where
+    service = Request.ServiceName "StepFunctions"
+    method = Request.MethodName "updateStateMachine"
 
 
 -- | <p>The specified activity does not exist.</p>
